562eaee75dca00647483dec9b1b9e8378b0b3294
wormh01e <DefineMyself2015@Gmail.com>
2025-08-02 16:08:26 +0800

    add base game ability, ready to spawn weapon

---
+++ Source/Warrior/Private/AbilityStystem/Abilities/WarriorGameplayAbility.cpp
@@ -0,0 +1,33 @@
+// Advanced Action RPG Learning, Wormhole All Rights Reserved
+
+
+#include "AbilityStystem/Abilities/WarriorGameplayAbility.h"
+
+#include "AbilitySystemComponent.h"
+
+void UWarriorGameplayAbility::OnGiveAbility(const FGameplayAbilityActorInfo* ActorInfo,
+	const FGameplayAbilitySpec& Spec)
+{
+	Super::OnGiveAbility(ActorInfo, Spec);
+	if (AbilityActivationPolicy == EWarriorAbilityActivationPolicy::OnGiven)
+	{
+		if (ActorInfo && !Spec.IsActive())
+		{
+			ActorInfo->AbilitySystemComponent->TryActivateAbility(Spec.Handle);
+		}
+	}
+}
+
+void UWarriorGameplayAbility::EndAbility(const FGameplayAbilitySpecHandle Handle,
+	const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo,
+	bool bReplicateEndAbility, bool bWasCancelled)
+{
+	Super::EndAbility(Handle, ActorInfo, ActivationInfo, bReplicateEndAbility, bWasCancelled);
+	if (AbilityActivationPolicy == EWarriorAbilityActivationPolicy::OnGiven)
+	{
+		if (ActorInfo)
+		{
+			ActorInfo->AbilitySystemComponent->ClearAbility(Handle);
+		}
+	}
+}
