4da576f5bc2a80eb3e28d9671c31a08442459b49
wormh01e <DefineMyself2015@Gmail.com>
2025-08-04 21:41:40 +0800

    use gameplayTag(status) to communicate between abilities: can directly heavyAttack2 after lightAttack3(input reaction not smooth)

--- Source/Warrior/Private/WarriorGameplayTags.cpp
+++ Source/Warrior/Private/WarriorGameplayTags.cpp
@@ -23,5 +23,7 @@ namespace WarriorGameplayTags
 	UE_DEFINE_GAMEPLAY_TAG(Player_Weapon_Axe, "Player.Weapon.Axe");
 	UE_DEFINE_GAMEPLAY_TAG(PlAYER_Event_Equip_Axe, "Player.Event.Equip.Axe");
 	UE_DEFINE_GAMEPLAY_TAG(Player_Event_Unequip_Axe, "Player.Event.Unequip.Axe");
+
+	UE_DEFINE_GAMEPLAY_TAG(Player_Status_JumpToFinish, "Player.Status.JumpToFinish");
 }
 
