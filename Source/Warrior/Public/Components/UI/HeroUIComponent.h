81ba5b08fa7b0f192327b999021ce75d456cb357
wormh01e <DefineMyself2015@Gmail.com>
2025-08-07 17:36:01 +0800

    set equipped weapon Icon,tweak hero/enemy UI

--- Source/Warrior/Public/Components/UI/HeroUIComponent.h
+++ Source/Warrior/Public/Components/UI/HeroUIComponent.h
@@ -6,6 +6,7 @@
 #include "PawnUIComponent.h"
 #include "HeroUIComponent.generated.h"
 
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnEquippedWeaponChangedDelegate, TSoftObjectPtr<UTexture2D>, SoftWeaponIconTexture);
 
 UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
 class WARRIOR_API UHeroUIComponent : public UPawnUIComponent
@@ -15,4 +16,7 @@ class WARRIOR_API UHeroUIComponent : public UPawnUIComponent
 public:
 	UPROPERTY(BlueprintAssignable)
 	FOnPercentChangedDelegate OnRageChanged;
+
+	UPROPERTY(BlueprintAssignable, BlueprintCallable)
+	FOnEquippedWeaponChangedDelegate OnEquippedWeaponChanged;
 };
