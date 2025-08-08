fe8f66f43a2a4f4ba0683629549d7fc60bc2f91d
wormh01e <DefineMyself2015@Gmail.com>
2025-08-09 00:18:21 +0800

    use EQS to find location close to hero: use circle EQS generator, custom EQS context to get circle center

--- Source/Warrior/Private/Controllers/WarriorAIController.cpp
+++ Source/Warrior/Private/Controllers/WarriorAIController.cpp
@@ -73,7 +73,7 @@ ETeamAttitude::Type AWarriorAIController::GetTeamAttitudeTowards(const AActor& O
 {
 	const APawn* PawnToCheck = Cast<const APawn>(&Other);
 	const IGenericTeamAgentInterface* OtherTeamAgent = Cast<const IGenericTeamAgentInterface>(PawnToCheck->GetController());
-	if (OtherTeamAgent && OtherTeamAgent->GetGenericTeamId() != GetGenericTeamId())
+	if (OtherTeamAgent && OtherTeamAgent->GetGenericTeamId() < GetGenericTeamId())
 	{
 		return ETeamAttitude::Hostile;
 	}
@@ -82,18 +82,24 @@ ETeamAttitude::Type AWarriorAIController::GetTeamAttitudeTowards(const AActor& O
 
 void AWarriorAIController::OnEnemyPerceptionUpdated(AActor* Actor, FAIStimulus Stimulus)
 {
-	if (Stimulus.WasSuccessfullySensed() && Actor)
+	if (UBlackboardComponent* BlackboardComponent = GetBlackboardComponent())
 	{
-		// if (EnemyStateTreeAIComponent->GetSchema())
-		// {
-		// 	FStateTreeEvent Event;
-		// 	Event.Tag = WarriorGameplayTags::Enemy_Status_Alert;
-		// 	Event.Payload = FInstancedStruct::Make(FVector(Actor->GetActorLocation()));
-		// 	EnemyStateTreeAIComponent->SendStateTreeEvent(Event);	
-		// }
-		if (UBlackboardComponent* BlackboardComponent = GetBlackboardComponent())
+		if (!BlackboardComponent->GetValueAsObject((FName("TargetActor"))))
 		{
-			BlackboardComponent->SetValueAsObject(FName("TargetActor"), Actor);
+			if (Stimulus.WasSuccessfullySensed() && Actor)
+			{
+				BlackboardComponent->SetValueAsObject(FName("TargetActor"), Actor);
+			}
 		}
 	}
+	// if (Stimulus.WasSuccessfullySensed() && Actor)
+	// {
+	// 	if (EnemyStateTreeAIComponent->GetSchema())
+	// 	{
+	// 		FStateTreeEvent Event;
+	// 		Event.Tag = WarriorGameplayTags::Enemy_Status_Alert;
+	// 		Event.Payload = FInstancedStruct::Make(FVector(Actor->GetActorLocation()));
+	// 		EnemyStateTreeAIComponent->SendStateTreeEvent(Event);	
+	// 	}
+	// }
 }
